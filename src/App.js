d1b326f093870c007d94dc3898bb73203061a4bd
Ravicha <palm.ravicha@outlook.com>
2024-10-17 20:30:21 +0700

    add simple cal page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import MainRate from './table';
 import './App.css';
+import Conv from './cal'
 
 
 const NotFound = () => {
@@ -12,10 +13,13 @@ const App = () => {
   return (
     <Router>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <Link className="navbar-brand" to="/">Currency Converter</Link>
+        <Link className="navbar-brand" to="/">Exchange Table</Link>
+        <Link to="/cal">Converter</Link>
       </nav>
       <Switch>
         <Route path="/" exact component={MainRate} />
+        <Route path="/cal" component={Conv} />
+        <Route component={NotFound} />
       </Switch>
       <footer>
         <p>By Ravicha, Guided by Altcademy</p>
